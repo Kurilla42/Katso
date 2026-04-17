4d10f6384223025de145b1d25dcd93f08e6128e0
Anton Kolesnikov <sterlet.prod@gmail.com>
2026-04-17 12:12:35 +0000

    Увеличь размер картинки 2 еще на 20%

--- src/components/sections/NewMe.tsx
+++ src/components/sections/NewMe.tsx
@@ -103,7 +103,7 @@ const NewMe = () => {
           {/* Images are positioned relative to this container */}
           <div ref={contentWrapperRef} className="absolute inset-0 w-full h-full">
             {/* Image 1 */}
-            <div className="floating-image absolute w-[11.9vw] aspect-[1/1.2] bottom-[10%] left-[10%] transform -rotate-[7deg]">
+            <div className="floating-image absolute w-[13.1vw] aspect-[1/1.2] bottom-[10%] left-[10%] transform -rotate-[7deg]">
                 <Image
                     src="https://i.ibb.co/8D9VnyxZ/790b8980-17ce-4517-9d41-bd3a5008a7ef.jpg"
                     alt="Woman with beautiful hair"
@@ -114,7 +114,7 @@ const NewMe = () => {
             </div>
 
             {/* Image 2 */}
-            <div className="floating-image absolute w-[10.4vw] aspect-[1.2/1] top-[12%] right-[15%] transform rotate-[6deg]">
+            <div className="floating-image absolute w-[15vw] aspect-[1.2/1] top-[8%] right-[12%] transform rotate-[6deg]">
                 <Image
                     src="https://i.ibb.co/v6xqgN14/440cc9b2-04fe-40e8-83e7-84d0e84b820f.jpg"
                     alt="Beauty salon details"
@@ -125,7 +125,7 @@ const NewMe = () => {
             </div>
 
             {/* Image 3 */}
-            <div className="floating-image absolute w-[11.5vw] aspect-[1/1.2] top-[8%] left-[40%]">
+            <div className="floating-image absolute w-[11.5vw] aspect-[1/1.2] top-[5%] left-[38%]">
                 <Image
                     src="https://i.ibb.co/pvCXs1fm/5a0c5da2-c0e2-4de6-be9f-3390f376e3e1.jpg"
                     alt="Woman getting a beauty treatment"
